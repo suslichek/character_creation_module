f76fa131232eec635a805947274e3bb455fa2eb5
suslichek <ntokmakova2@gmail.com>
2025-11-04 20:38:14 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import Union
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -66,9 +66,9 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
-def choice_char_class() -> Union[str, None] :
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice = ''
+    char_class = ''
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь '
                            'играть: Воитель — warrior,'
@@ -92,16 +92,15 @@ def choice_char_class() -> Union[str, None] :
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
